af1cefa867097708235d0a73a6d6e4da6552e98e
Nick <bing900713@hotmail.com>
2023-06-20 18:56:05 +0800

    complete all the four functions, thus the demo makes sense

--- grpc/route-client/client.go
+++ grpc/route-client/client.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	pb "github.com/NickYxy/GoTypingTest/grpc/route"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"io"
 	"log"
+	"os"
+	"time"
 )
 
 func runFirst(client pb.RouteGuideClient) {
@@ -21,6 +26,98 @@ func runFirst(client pb.RouteGuideClient) {
 	 log.Println(feature)
 }
 
+func runSecond(client pb.RouteGuideClient) {
+	serverStream, err := client.ListFeatures(context.Background(), &pb.Rectangle{
+		Lo: &pb.Point{Latitude: 313374060, Longitude: 121358540},
+		Hi: &pb.Point{Latitude: 311034130, Longitude: 121598790},
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		feature, err := serverStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {log.Fatalln(err)}
+
+		fmt.Println(feature)
+	}
+}
+
+func runThird(client pb.RouteGuideClient) {
+	// dummy data
+	points := []*pb.Point{
+		{Latitude: 313374060, Longitude: 121358540},
+		{Latitude: 311034130, Longitude: 121598790},
+		{Latitude: 310235000, Longitude: 121437403},
+	}
+
+	clientStream, err := client.RecordRoute(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, point := range points {
+		if err := clientStream.SendMsg(point); err != nil {
+			log.Fatalln(err)
+		}
+		time.Sleep(time.Second)
+	}
+
+	summary, err := clientStream.CloseAndRecv()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(summary)
+}
+
+func runFourth(client pb.RouteGuideClient) {
+	stream, err := client.Recommend(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// this goroutine listen to the server stream
+	go func() {
+		feature, err2 := stream.Recv()
+		if err2 != nil {
+			log.Fatalln(err2)
+		}
+		fmt.Println("Recommended", feature)
+	}()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		request := pb.RecommendationRequest{Point: new(pb.Point)}
+		var mode int32
+		fmt.Print("Enter Recommendation Mode (0 for nearest, 1 for the farthest")
+
+		readIntFromCommandLine(reader, &mode)
+		fmt.Print("Enter Latitude: ")
+		readIntFromCommandLine(reader, &request.Point.Latitude)
+		fmt.Print("Enter Longitude: ")
+		readIntFromCommandLine(reader, &request.Point.Longitude)
+		request.Mode = pb.RecommendationMode(mode)
+
+		if err := stream.Send(&request); err != nil {
+			log.Fatalln(err)
+		}
+
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func readIntFromCommandLine(reader *bufio.Reader, target *int32) {
+	_, err := fmt.Fscanf(reader, "%d\n", target)
+	if err != nil {
+		log.Fatalln("Cannot scan", err)
+	}
+}
+
 func main() {
 	conn, err := grpc.Dial("localhost:5000", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
 	if err != nil {
@@ -30,5 +127,8 @@ func main() {
 
 	client := pb.NewRouteGuideClient(conn)
 
-	runFirst(client)
+	//runFirst(client)
+	//runSecond(client)
+	//runThird(client)
+	runFourth(client)
 }
