175877f6844a4a8a8cd5b34bd4c916877a671a9d
Nick <bing900713@hotmail.com>
2023-08-03 19:51:22 +0800

    配合Wg实现在指定时间内只有访问特定次数的简单限流策略

--- templates/concurrent_ipvistier.go
+++ templates/concurrent_ipvistier.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -23,15 +24,32 @@ func (b *Ban) visit(ip string) bool {
 		return true
 	}
 	b.visitIPs[ip] = time.Now()
+	go b.delMap(ip)
 	return false
+}
+
+func (b *Ban) delMap(ip string) {
+	timer := time.NewTicker(time.Second * 1)
+	select {
+	case <-timer.C:
+		b.Lock()
+		defer b.Unlock()
+		delete(b.visitIPs, ip)
+		fmt.Printf("我们删除了ip: %s \n", ip)
+	}
 }
 
 func main() {
 	var success int64 = 0
 	ban := NewBan()
+	wait := sync.WaitGroup{}
+	wait.Add(1000 * 100)
 	for i := 0; i < 1000; i++ {
 		for j := 0; j < 100; j++ {
 			go func(j int) {
+				defer wait.Done()
+				waitTime := time.Duration(rand.Int63n(2))
+				time.Sleep(time.Second * waitTime)
 				ip := fmt.Sprintf("192.168.0.%d", j)
 				if !ban.visit(ip) {
 					atomic.AddInt64(&success, 1)
@@ -39,6 +57,6 @@ func main() {
 			}(j)
 		}
 	}
-
+	wait.Wait()
 	fmt.Printf("success is: %d", success)
 }
